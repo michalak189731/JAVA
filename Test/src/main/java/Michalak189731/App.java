21609876b6f9fab84952877efc194011877c34de
Mateusz Michalak <Mateusz Michalak>
2017-03-01 10:59:47 +0100

    Initial Project with test

---
+++ Test/src/main/java/Michalak189731/App.java
@@ -0,0 +1,13 @@
+package Michalak189731;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test/src/test/java/Michalak189731/superTest.java
@@ -0,0 +1,15 @@
+package Michalak189731;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class superTest {
+
+	@Test
+	public void test() {
+		assertThat(true).isEqualTo(true);
+	}
+
+}
