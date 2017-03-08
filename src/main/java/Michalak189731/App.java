6eff1c789d615dda1b4362fddb078b52e400d0a8
Mateusz Michalak <Mateusz Michalak>
2017-03-08 08:47:31 +0100

    TEST

--- src/main/java/Michalak189731/App.java
+++ src/main/java/Michalak189731/App.java
@@ -15,6 +15,6 @@ public class App
     public void SayHello(){
     	System.out.println("Hi");
     	System.out.print("Hello");
-    	System.out.println("Goodbye");
+    	System.out.println("Derp");
     }
 }
