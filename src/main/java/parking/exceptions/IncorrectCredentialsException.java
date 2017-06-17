7f4b713bfc9d6d425ba51aeb583fedb10cbfa952
Majos <krzysztofjanmajewski@gmail.com>
2017-06-17 04:58:35 +0200

    Added CurrentlyRenting and IncorrectCredentials exceptions

---
+++ src/main/java/parking/exceptions/IncorrectCredentialsException.java
@@ -0,0 +1,10 @@
+package parking.exceptions;
+
+/**
+ * Created by emazmik on 2017-06-14.
+ */
+public class IncorrectCredentialsException extends Exception {
+    public IncorrectCredentialsException(String message) {
+        super(message);
+    }
+}
