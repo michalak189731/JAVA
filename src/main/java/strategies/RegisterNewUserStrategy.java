254f45a6dc9dbe9831be641d1e33a9f73b1ffb5e
michalak189731 <mder808@wp.pl>
2017-06-19 20:00:57 +0200

    Adding lost strategy

---
+++ src/main/java/strategies/RegisterNewUserStrategy.java
@@ -0,0 +1,42 @@
+package strategies;
+
+import java.util.logging.Logger;
+
+import bots.Bot;
+import bots.Credential;
+import bots.CredentialBroker;
+import parking.Parking;
+import parking.Person;
+import strategies.AbstractStrategy;
+
+public class RegisterNewUserStrategy implements AbstractStrategy {
+
+	private static int IDOfNextUser = 1;
+	
+	
+	@Override
+	public synchronized void execute(Bot user, Parking parking) {
+		String newUserLogin = "user" + IDOfNextUser;
+		IDOfNextUser++;
+		String newUserPassword = "mypass";
+		
+		
+		Logger strategyLogger = Logger.getLogger(StrategyLoggerName);
+		
+		strategyLogger.info(user + "trying to register user " + newUserLogin);
+		
+		Person newPerson = parking.RegisterPerson("Tom", "Baker", "tombaker@whatnot.com", newUserLogin, newUserPassword, null, false, false);
+		if(newPerson==null)
+		{
+			strategyLogger.warning(user + "couldn't register user canceling...");
+			return;
+		}
+		
+		Credential cred = new Credential(newUserLogin, newUserPassword);
+		CredentialBroker.getInstance().returnCredential(cred);
+		
+		strategyLogger.info("Successfuly registered user " + newUserLogin);
+
+	}
+
+}
